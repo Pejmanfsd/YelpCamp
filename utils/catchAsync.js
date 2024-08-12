69d1670f00272086da0622344588517bd6f9c144
Pejmanfsd <124799016+Pejmanfsd@users.noreply.github.com>
2024-08-12 11:38:25 -0400

    Delete utils directory

--- utils/catchAsync.js
+++
@@ -1,8 +0,0 @@
-// Step 23(B): In order to avoid the duplicated codes,
-// we create a function that accepts another function
-// and returns a new function with "req", "res" and "next" as its inputs
-module.exports = func => {
-    return (req, res, next) => {
-        func(req, res, next).catch(next);
-    }
-}
